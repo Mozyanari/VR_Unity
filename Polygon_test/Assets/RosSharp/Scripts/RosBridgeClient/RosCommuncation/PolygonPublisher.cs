f9cf08f30f1592aec1cab368fd42d999c9d54475
LabPC <mozyanari@outloo.jp>
2019-07-23 17:55:14 +0900

    ROS#のテスト

---
+++ Polygon_test/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/PolygonPublisher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PolygonPublisher : Publisher<Messages.Geometry.Polygon>
+    {
+        Messages.Geometry.Polygon message;
+
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void InitializeMessage()
+        {
+            message = new Messages.Geometry.Polygon { points = new Messages.Geometry.Point[1] };
+            message.points[0] = new Messages.Geometry.Point();
+        }
+
+        private void UpdateMessage()
+        {
+            message.points[0].x = (float)1.0f;
+            message.points[0].y = (float)2.0f;
+            message.points[0].z = (float)3.0f;
+
+            Publish(message);
+        }
+
+        private void Update()
+        {
+            UpdateMessage();
+        }
+    }
+}
+
