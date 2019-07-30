904fe20f323316ab316c49ad7ef7754fe743fa61
LabPC <mozyanari@outloo.jp>
2019-07-30 15:26:27 +0900

    meshの結合を動的に生成できるようになった

--- EditGridMap/Assets/MyScripts/MeshCombineOccupancyGridSubscriber.cs
+++ EditGridMap/Assets/MyScripts/MeshCombineOccupancyGridSubscriber.cs
@@ -16,7 +16,7 @@ namespace RosSharp.RosBridgeClient
         private int height;
         //横方向のcell数
         private int width;
-        //
+        //mapの原点に対するオフセット
         private float offset_x;
         private float offset_y;
 
@@ -31,27 +31,34 @@ namespace RosSharp.RosBridgeClient
         private float[] grids;
         private GameObject base_grid;
         private GameObject[] GridPoint;
-        private GameObject Point_1;
-        private GameObject Point_2;
+        
+        
         private Vector3[] GridPosition;
         private Vector3 Position;
-        private bool[] GridFlag;
-        private bool[] old_GridFlag;
-        private Dictionary<uint, GameObject> GridList = new Dictionary<uint, GameObject>();
-        private List<uint> change_number = new List<uint>();
+        private sbyte[] GridFlag;
+        private sbyte[] old_GridFlag;
 
-        //デバック変数
-        //障害物の数
-        private int count = 0;
-        //一つ目前障害物の数
-        private int old_count = 0;
+        //mapのmeshデータ
+        private GameObject[] map_mesh;
+        private int cube_max;
+        private int cube_count;
+
+        //障害物の数，床の数
+        private int wall_quantity = 0;
+        private int floor_quantity = 0;
+
+        //表示する結合したメッシュ
+        private Mesh[] combineMesh;
+
+        //robot位置
+        public GameObject RobotObject;
 
         // Use this for initialization
         protected override void Start()
         {
             base.Start();
-            GridFlag = new bool[0];
-            old_GridFlag = new bool[0];
+            GridFlag = new sbyte[0];
+            old_GridFlag = new sbyte[0];
         }
 
         private void FixedUpdate()
@@ -59,9 +66,8 @@ namespace RosSharp.RosBridgeClient
             if (receive_flag == 1)
             {
                 createmap();
-
                 //一つ前の障害物のデータを比較用に保存
-                old_GridFlag = GridFlag;
+                //old_GridFlag = GridFlag;
             }
 
         }
@@ -71,231 +77,68 @@ namespace RosSharp.RosBridgeClient
 
         }
 
-        //得られたデータから地図の位置にCubeを置く
-        //old_GridFlagとGridFlagの長さが違うときは，最初の地図の更新または異なる地図の受信と考えてすべてのcubeを破壊して再生成
-        //また，(数字,gameobject)のリストですべてを保持して変更する際には数字を参照する
-        //同じ場合は変更がある場所だけ，破壊・生成を行う
         private void createmap()
         {
-            //GridFlagからCubeを作成
-            Point_1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Point_2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //Point.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-            //Point.transform.localScale = new Vector3(resolution, resolution, resolution);
-            //GameObject.Destroy(Point.GetComponent<BoxCollider>());
+            //受信フラグを折る
+            receive_flag = 0;
+            //使用するmeshを取得
+            Mesh cubeMesh = GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<MeshFilter>().sharedMesh;
 
-            GameObject Point_MESH = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //Point_MESH.transform.localScale = new Vector3((float)resolution, (float)resolution, (float)resolution);
-            Mesh cubeMesh = Point_MESH.GetComponent<MeshFilter>().sharedMesh;
-            Point_MESH.SetActive(false);
-            Debug.Log(cubeMesh.vertexCount);
-            //cubeMesh.bounds.Expand(0.01f);
+            //meshの頂点の数からcube_maxを設定
+            cube_max = (65536 / cubeMesh.vertexCount);
+            //メッシュを何個に分割すればいいか計算
+            int mesh_count = (wall_quantity / cube_max) + 1;
 
-            CombineInstance[] combineInstanceAry_1 = new CombineInstance[2000];
-            CombineInstance[] combineInstanceAry_2 = new CombineInstance[1725];
-
-            int j = 0;
-            for (uint i = 0; i < data_length; i++)
+            //mesh_countからCombineInstanceを動的生成
+            CombineInstance[][] combineInstanceAry = new CombineInstance[mesh_count][];
+            for(int i=0;i<mesh_count;i++)
             {
-                if (GridFlag[i] == true)
+                combineInstanceAry[i] = new CombineInstance[cube_max];
+            }
+
+            int wall_count = 0;
+            int floor_count = 0;
+            for (int i = 0; i < data_length; i++)
+            {
+                if (GridFlag[i] == 1)
                 {
+                    Debug.Log(wall_count);
                     //位置の計算
                     //行の番号
-                    int line = (int)i / width;
+                    int line = i / width;
                     //列の番号
-                    int raw = (int)i % width;
-                    //行の位置の計算
-                    //GridPosition[j].x = (line * resolution) - ((resolution * width)/2);
-                    //Position.z = ((resolution / 2) + (line) * resolution)-20;
+                    int raw = i % width;
+                    //行列の位置の計算
                     Position.y = ((resolution / 2) + (line) * resolution) + offset_y;
-                    //Position.x = ((resolution / 2) + (line) * resolution);
-                    //列の位置の計算
-                    //GridPosition[j].z = (raw * resolution) - ((resolution * height)/2);
                     Position.x = ((resolution / 2) + (raw) * resolution) + offset_x;
-                    //Position.y = ((resolution / 2) + (raw) * resolution);
                     Position.z = 0;
                     //ROSの座標系からUnityの座標系に変換
                     Position = Position.Ros2Unity();
-                    if (j < 2000)
-                    {
-                        combineInstanceAry_1[j].mesh = cubeMesh;
-                        combineInstanceAry_1[j].transform = Matrix4x4.TRS(new Vector3(Position.x, 0, Position.z), Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f)), new Vector3(resolution, resolution, resolution));
-                    }
-                    else
-                    {
-                        combineInstanceAry_2[j-2000].mesh = cubeMesh;
-                        combineInstanceAry_2[j-2000].transform = Matrix4x4.TRS(new Vector3(Position.x, 0, Position.z), Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f)), new Vector3(resolution, resolution, resolution));
-                    }
-                    
 
-                    j++;
+                    //CombineInstanceに情報を入力
+                    combineInstanceAry[wall_count / cube_max][wall_count % cube_max].mesh = cubeMesh;
+                    combineInstanceAry[wall_count / cube_max][wall_count % cube_max].transform = Matrix4x4.TRS(new Vector3(Position.x, 0, Position.z), Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f)), new Vector3(resolution, resolution, resolution));
 
-                    /*
-                    //Objectの生成
-                    Point = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    //Colliderの当たり判定の削除
-                    //DestroyImmediate(Point.GetComponent<Collider>());
-                    //nameはiを参照して設定
-                    Point.name = "GridPoint" + i;
-                    //tagはGridで設定
-                    Point.tag = "Grid";
-                    //このスクリプトが張られたオブジェクトを親にして，今回生成したオブジェクトが子になる
-                    Point.transform.parent = transform;
-                    //色をランダムに
-                    //Point.GetComponent<Renderer>().material.color = new Color((float)(i % Random.Range(1.0f, 100.0f) * 0.01), (float)(i % Random.Range(1.0f, 100.0f) * 0.01), (float)(i % Random.Range(1.0f, 100.0f) * 0.01));
-                    //Point.GetComponent<Renderer>().material.color = new Color((float)0, (float)0, (float)0);
-                    Point.transform.localScale = new Vector3((float)resolution, (float)resolution, (float)resolution);
-                    //位置は上で計算済みのものを使用
-                    Point.transform.localPosition = Position;
+                    wall_count++;
 
-                    //Listに追加
-                    GridList.Add(i, Point);
-                    */
-                    
                 }
-                
+                else if (GridFlag[i] == 0)
+                {
+                    //todo:メッシュが決定次第実装
+                }
             }
-            Debug.Log(j);
-            var combinedMesh_1 = new Mesh();
-            var combinedMesh_2 = new Mesh();
-            combinedMesh_1.name = "Cubes_1";
-            combinedMesh_2.name = "Cubes_2";
-            combinedMesh_1.CombineMeshes(combineInstanceAry_1);
-            combinedMesh_2.CombineMeshes(combineInstanceAry_2);
-            // 上書きする
-            Point_1.GetComponent<MeshFilter>().mesh = combinedMesh_1;
-            Point_2.GetComponent<MeshFilter>().mesh = combinedMesh_2;
 
-            Debug.Log(combinedMesh_1.vertexCount);
-            Debug.Log(combinedMesh_1.vertexBufferCount);
-
-            receive_flag = 0;
-            /*
-            if (old_GridFlag.Length != GridFlag.Length)
+            Mesh[] combineMesh = new Mesh[mesh_count];
+            map_mesh = new GameObject[mesh_count];
+            for (int i = 0; i< mesh_count; i++)
             {
-                //Gridタグのオブジェクトをすべて破壊
-                var clones = GameObject.FindGameObjectsWithTag("Grid");
-                if (clones != null)
-                {
-                    foreach (var clone in clones)
-                    {
-                        Destroy(clone);
-                    }
-                }
-                //Listも初期化
-                GridList.Clear();
-
-                //GridFlagからCubeを作成
-                for (uint i = 0; i < data_length; i++)
-                {
-                    if (GridFlag[i] == true)
-                    {
-                        //位置の計算
-                        //行の番号
-                        int line = (int)i / width;
-                        //列の番号
-                        int raw = (int)i % width;
-                        //行の位置の計算
-                        //GridPosition[j].x = (line * resolution) - ((resolution * width)/2);
-                        //Position.z = ((resolution / 2) + (line) * resolution)-20;
-                        Position.y = ((resolution / 2) + (line) * resolution) + offset_y;
-                        //Position.x = ((resolution / 2) + (line) * resolution);
-                        //列の位置の計算
-                        //GridPosition[j].z = (raw * resolution) - ((resolution * height)/2);
-                        Position.x = ((resolution / 2) + (raw) * resolution) + offset_x;
-                        //Position.y = ((resolution / 2) + (raw) * resolution);
-                        Position.z = 0;
-                        Position = Position.Ros2Unity();
-
-                        //Objectの生成
-                        Point = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        //Colliderの当たり判定の削除
-                        //DestroyImmediate(Point.GetComponent<Collider>());
-                        //nameはiを参照して設定
-                        Point.name = "GridPoint" + i;
-                        //tagはGridで設定
-                        Point.tag = "Grid";
-                        //このスクリプトが張られたオブジェクトを親にして，今回生成したオブジェクトが子になる
-                        Point.transform.parent = transform;
-                        //色をランダムに
-                        //Point.GetComponent<Renderer>().material.color = new Color((float)(i % Random.Range(1.0f, 100.0f) * 0.01), (float)(i % Random.Range(1.0f, 100.0f) * 0.01), (float)(i % Random.Range(1.0f, 100.0f) * 0.01));
-                        //Point.GetComponent<Renderer>().material.color = new Color((float)0, (float)0, (float)0);
-                        Point.transform.localScale = new Vector3((float)resolution, (float)resolution, (float)resolution);
-                        //位置は上で計算済みのものを使用
-                        Point.transform.localPosition = Position;
-
-                        //Listに追加
-                        GridList.Add(i, Point);
-                    }
-                }
+                combineMesh[i] = new Mesh();
+                combineMesh[i].name = "Mesh" + i;
+                combineMesh[i].CombineMeshes(combineInstanceAry[i]);
+                map_mesh[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                map_mesh[i].GetComponent<MeshFilter>().mesh = combineMesh[i];
             }
-            else
-            {
-                for (uint i = 0; i < data_length; i++)
-                {
-                    if (old_GridFlag[i] != GridFlag[i])
-                    {
-                        if (GridFlag[i] == true)
-                        {
-                            //位置の計算
-                            //行の番号
-                            int line = (int)i / width;
-                            //列の番号
-                            int raw = (int)i % width;
-                            //行の位置の計算
-                            //GridPosition[j].x = (line * resolution) - ((resolution * width)/2);
-                            //Position.z = ((resolution / 2) + (line) * resolution)-20;
-                            Position.y = ((resolution / 2) + (line) * resolution) + offset_y;
-                            //Position.x = ((resolution / 2) + (line) * resolution);
-                            //列の位置の計算
-                            //GridPosition[j].z = (raw * resolution) - ((resolution * height)/2);
-                            Position.x = ((resolution / 2) + (raw) * resolution) + offset_x;
-                            //Position.y = ((resolution / 2) + (raw) * resolution);
-                            Position.z = 0;
-                            Position = Position.Ros2Unity();
-
-                            //Objectの生成
-                            Point = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                            //Colliderの当たり判定の削除
-                            DestroyImmediate(Point.GetComponent<Collider>());
-                            //nameはiを参照して設定
-                            Point.name = "GridPoint" + i;
-                            //tagはGridで設定
-                            Point.tag = "Grid";
-                            //このスクリプトが張られたオブジェクトを親にして，今回生成したオブジェクトが子になる
-                            Point.transform.parent = transform;
-                            //色をランダムに
-                            Point.GetComponent<Renderer>().material.color = new Color((float)(i % Random.Range(1.0f, 100.0f) * 0.01), (float)(i % Random.Range(1.0f, 100.0f) * 0.01), (float)(i % Random.Range(1.0f, 100.0f) * 0.01));
-                            Point.transform.localScale = new Vector3((float)resolution, (float)resolution, (float)resolution);
-                            //位置は上で計算済みのものを使用
-                            Point.transform.localPosition = Position;
-
-                            //Listに追加
-                            GridList.Add(i, Point);
-                        }
-                        else
-                        {
-
-                            //Debug.Log("single destroy");
-                            //cubeを破壊
-                            Destroy(GameObject.Find("GridPoint" + i));
-                            //List上でも破壊
-                            GridList.Remove(i);
-
-                        }
-
-                    }
-                    else
-                    {
-                        //何もしない
-                    }
-
-                }
-            }
-            receive_flag = 0;
-            */
         }
-
 
         protected override void ReceiveMessage(Messages.Navigation.OccupancyGrid gridmap)
         {
@@ -314,25 +157,32 @@ namespace RosSharp.RosBridgeClient
             //Debug.Log(width);
             //data数の取得
             data_length = gridmap.data.Length;
-            //GridPoint = new GameObject[gridmap.data.Length];
-            GridFlag = new bool[gridmap.data.Length];
-            //grids[i]が100以上なら壁なのでGridFlagにtrueを入れる
-            count = 0;
+
+            //データ数に応じて，地図情報を保存する変数を動的生成
+            GridFlag = new sbyte[data_length];
+
+            //grids[i]が100以上なら壁，0なら床，それ以外は何もなし
+            wall_quantity = 0;
+            floor_quantity = 0;
             for (uint i = 0; i < data_length; i++)
             {
                 //Debug.Log(i);
                 if (gridmap.data[i] > 99)
                 {
-                    GridFlag[i] = true;
-                    count++;
+                    GridFlag[i] = 1;
+                    wall_quantity++;
+                }else if(gridmap.data[i] == 0)
+                {
+                    GridFlag[i] = 0;
+                    floor_quantity++;
                 }
                 else
                 {
-                    GridFlag[i] = false;
+                    GridFlag[i] = -1;
                 }
             }
             receive_flag = 1;
-            Debug.Log(count);
+            //Debug.Log(count);
         }
     }
 }
