028d99c418bdbe399f1d6aff630ebf0a97a3c02d
LabPC <mozyanari@outloo.jp>
2019-10-22 17:53:05 +0900

    デバック用の表示を追加

--- PointCloud/Assets/PointCloud.cs
+++ PointCloud/Assets/PointCloud.cs
@@ -7,7 +7,7 @@ public class PointCloud : MonoBehaviour
 {
 
     private Mesh mesh;
-    int numPoints = 600000;
+    int numPoints = 6000;
     float size = 0;
     // Use this for initialization
     void Start()
@@ -19,10 +19,30 @@ public class PointCloud : MonoBehaviour
 
         
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        size += 0.001f;
-        GetComponent<Renderer>().material.SetFloat("_PointSize", size);
+        size += 0.01f;
+
+        Vector3[] points = new Vector3[numPoints];
+        int[] indecies = new int[numPoints];
+        Color[] colors = new Color[numPoints];
+        for (int i = 0; i < points.Length; ++i)
+        {
+            points[i] = new Vector3(Random.Range(-size, size), Random.Range(-size, size), Random.Range(-size, size));
+            indecies[i] = i;
+            colors[i] = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+        }
+
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        mesh.vertices = points;
+        mesh.colors = colors;
+        mesh.SetIndices(indecies, MeshTopology.Points, 0);
+
+        GetComponent<Renderer>().material.SetFloat("_PointSize", 10.0f);
+        if (size > 10)
+        {
+            size = 0;
+        }
     }
 
     void CreateMesh()
